e377214d35a0d3f578de024d1cc164448c7b3429
harrixon <harrisonchan1991@gmail.com>
2018-03-19 23:50:26 +0800

    hard code, auto fit in canvas

--- assets/js/drawing-background.js
+++ assets/js/drawing-background.js
@@ -1,17 +1,10 @@
-class DrawingBackground extends PaintFunction{
-    constructor(contextReal){
-        super();
-        // this.contextBackground = contextBackground;
-        this.contextReal = contextReal;
-        this.setBackground();
-    }
-    
-    setBackground(event){
-        var img = new Image();
-        img.src = "assets/img/background.png";
-        img.width = 10;
-        this.contextReal.drawImage(img, 0, 0);
-    }
+// class DrawingBackground extends PaintFunction{
+//     constructor(contextReal){
+//         super();
+//         // this.contextBackground = contextBackground;
+//         this.contextReal = contextReal;
+//         this.setBackground();
+//     }
 
     // onMouseDown(){}
     // onDragging(){}
@@ -19,5 +12,22 @@ class DrawingBackground extends PaintFunction{
     // onMouseUp(){}
     // onMouseLeave(){}
     // onMouseEnter(){}
+// }
+    
+function setBackground(){
+    var img = new Image();
+    img.src = "assets/img/gate.jpg";
+    img.onload = function(){
+        var w = canvasReal.width;
+        var h = img.height / (img.width / w);
+        if (h > canvasReal.height)
+        {
+            h = canvasReal.height;
+            w = img.width / (img.height / h);
+        }
+        // console.log(w, h);
+        contextReal.drawImage(img, 0, 0, w, h);
+    }
+    cPush();
 }
 
